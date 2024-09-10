d1efb568c5e201f9c3cbc17eecbecaddd30ae6fe
Alfred <alfred-brannarer@hotmail.com>
2024-09-10 15:21:34 +0200

    Centered the button and h1 element

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function clickMe() {
     const h1Element = document.getElementById("changeH1Element");
 
     // Checks the current text of h1 inside index.html and does the loop.
-    if (h1Element.textContent === "Hello World") { //Is h1Element === "Hello World"?
+    if (h1Element.textContent === "Hello World") { //Is the text inside h1Element "Hello World"?
         h1Element.innerHTML = "Why did you click the button!?"; //Change to "Why did you click..."
-    } else { //Is h1Element === "Why did you click..."
+    } else { 
         h1Element.innerHTML = "Hello World"; //Change to "Hello World"
     }
 }
