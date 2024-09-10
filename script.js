9cd2eb4ab33dc31ff563d5628099bef67a12c95b
Alfred <alfred-brannarer@hotmail.com>
2024-09-10 14:18:27 +0200

    Some more tweaks with the help of w3schools and tips from classmates

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function clickMe(){
-    document.getElementById("changeH1Element").innerHTML = "Why did you click the button! "
+function clickMe() {
+    // Looks at the h1 element
+    const h1Element = document.getElementById("changeH1Element");
+
+    // Checks the current text of h1 inside index.html and does the loop.
+    if (h1Element.textContent === "Hello World") { //Is h1Element === "Hello World"?
+        h1Element.innerHTML = "Why did you click the button!?"; //Change to "Why did you click..."
+    } else { //Is h1Element === "Why did you click..."
+        h1Element.innerHTML = "Hello World"; //Change to "Hello World"
+    }
 }
