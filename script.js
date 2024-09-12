493b92bececd53dd56a1c16bcd9df213b24f2f91
Alfred <alfred-brannarer@hotmail.com>
2024-09-12 12:16:23 +0200

    Updated to querySelector

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function clickMe() {
     // Looks at/finds the elemet with id = changeH1Element and stores it in h1Element
-    const h1Element = document.getElementById("changeH1Element");
+    const h1Element = document.querySelector("#changeH1Element");
 
     // Checks the current text of h1 inside index.html and does the loop.
     if (h1Element.textContent === "Hello World") { //Is the text inside h1Element "Hello World"?
