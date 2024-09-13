bfe93a125ba034bd36f317fc59dfe89d2dbb6c60
Alfred <alfred-brannarer@hotmail.com>
2024-09-13 15:41:08 +0200

    Changed text to "Goodbye World"

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function clickMe() {
 
     // Checks the current text of h1 inside index.html and does the loop.
     if (h1Element.textContent === "Hello World") { //Is the text inside h1Element "Hello World"?
-        h1Element.innerHTML = "Why did you click the button!?"; //Change to "Why did you click..."
+        h1Element.innerHTML = "Goodbye World"; //Change to "Why did you click..."
     } else { 
         h1Element.innerHTML = "Hello World"; //Change to "Hello World"
     }
